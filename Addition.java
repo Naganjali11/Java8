278776874da6e66b8c843a853b09b7c15bbc1bee
Naganjali11 <naganjali425@gmail.com>
2025-10-28 21:08:43 +0530

    javafullstackprograms

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package com.codegnan.java8;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		Interef i=(a,b)->System.out.println(a+b);
+		i.sum(13,20);
+		i.sum(18,30);
+	}
+
+}
