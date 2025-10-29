0badbf20d40aac331c7ba497428aff7188376627
Naganjali11 <naganjali425@gmail.com>
2025-10-29 16:35:35 +0530

    javafullstackprograms

---
+++ AssertionExample.java
@@ -0,0 +1,11 @@
+package com.codegnan.java8;
+
+public class AssertionExample {
+
+	public static void main(String[] args) {
+		int x=20;
+		assert(x>10):"here x value should be greater than 10";
+		System.out.println(x);
+	}
+
+}
